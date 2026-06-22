e39799b2547484bc034d0906c71826fa848d7fd4
manggala56 <manggalaputra740@gmail.com>
2026-06-22 08:39:13 +0700

    add fillable to restock model

--- app/Models/Restock.php
+++ app/Models/Restock.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restock extends Model
 {
-    //
+    protected $fillable = [
+        'tenant_id',
+        'product_id',
+        'raw_material_id',
+        'quantity',
+        'unit_cost',
+        'total_cost',
+        'supplier_name',
+        'restock_date',
+        'expired_date',
+        'notes',
+    ];
 }
